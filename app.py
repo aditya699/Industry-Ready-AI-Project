e2ba87f7645bbaeea505d3f083861c910e3205e1
aditya <meenakshi.bhtt@gmail.com>
2024-08-30 11:58:35 +0530

    added dummy gemini implementation

--- app.py
+++ app.py
@@ -1,10 +1,36 @@
+'''
+Author -Aditya Bhatt 11:50 AM 30-08-2024
 
-from flask import Flask
+TODO:Create a simple chating system with gemini llm
+'''
 
-app = Flask(__name__)
+import os
+from dotenv import load_dotenv
+from langchain_google_genai import ChatGoogleGenerativeAI
 
-# Add your routes and logic here
+# Load environment variables from .env file
+load_dotenv()
 
-if __name__ == "__main__":
-    app.run(debug=True)
-    
+# Access environment variables
+
+os.environ["GOOGLE_API_KEY"] = os.getenv("GEMINI_API_KEY")
+
+
+llm = ChatGoogleGenerativeAI(
+    model="gemini-1.5-flash",
+    temperature=0,
+    max_tokens=None,
+    timeout=None,
+    max_retries=2,
+    # other params...
+)
+
+messages = [
+    (
+        "system",
+        "You are a helpful assistant that translates English to Hindi and Tamil. Translate the user sentence.",
+    ),
+    ("human", "I love programming."),
+]
+ai_msg = llm.invoke(messages)
+print(ai_msg.content)
